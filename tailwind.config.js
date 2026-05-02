d47d21bf0ff07054646fa252ce2f559d96722144
cauajucadiniz <cauandiniz12@gmail.com>
2026-05-02 18:45:47 -0300

    Add Tailwind CSS configuration file
    Configure Tailwind CSS with custom amber colors.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        amber: {
+          500: '#D4AF37',
+          600: '#9A7A30',
+        }
+      },
+    },
+  },
+  plugins: [],
+}
